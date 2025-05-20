b4024b4f34992be967fcf1a7a46a2c2e9db7227d
Taimoor Hamza <taimoorhamza1999@gmail.com>
2025-05-20 20:34:47 +0400

    dsfsd

--- database.js
+++ database.js
@@ -1,14 +1,18 @@
+const fs = require('fs');
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const dbUrl = new URL(process.env.MYSQL_URL);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace(/^\//, ''), // removes leading slash
+  port: dbUrl.port || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: dbUrl.searchParams.has('ssl') ? { rejectUnauthorized: false } : undefined,
 });
 
 async function initializeDatabase() {
